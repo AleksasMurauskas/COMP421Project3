5241c79c09430a005a89fd4647a794a5b87a17b7
AleksasMurauskas <aleksmurauskas@gmail.com>
2020-04-12 19:11:25 -0400

    Allows exit from program

--- project.py
+++ project.py
@@ -27,8 +27,11 @@ def main():
 	while(true): #infinite loop for user inputs 
 		
 		use_in= str(input())
-		
-		if(int(use_in)==0): #prompted exit 
+		if(int(use_in)==0): #prompted menu to be displayed  
+			display_menu()
+		if(int(use_in)==10): # prompted exit 
+			exit_prog()
+
 
 
 
@@ -45,12 +48,13 @@ def exit_prog():
 
 def display_menu():
 	print("-----------------Program Menu-----------------")
-	print("Enter the associted number to begin a feature:")
-	print("0: Exit Program")
+	print("Enter the associted number to begin a feature:")	
+	print("0: Redisplay Menu")
 	print("1: Query 1")
 	print("2: Query 2")
 	print("3: Query 3")
 	print("4: Query 4")
 	print("5: Query 5")
+	print("10: Exit Program")
 
 
